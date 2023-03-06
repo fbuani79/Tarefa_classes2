2906237c2b488c7be38e20d0f5e896004b9581e4
Fabio <109042577+fbuani79@users.noreply.github.com>
2023-03-06 20:30:38 -0300

    Add files via upload

---
+++ TarefaCalculoMedia.java
@@ -0,0 +1,45 @@
+
+
+
+
+public class TarefaCalculoMedia {
+
+    public static void main(String args[]) {
+
+        int nota1 = 8;
+        int nota2 = 6;
+        int nota3 = 7;
+        int nota4 = 5;
+
+        int num1 = nota1 + nota2 + nota3 + nota4;
+
+        System.out.println(num1);
+
+        int media = num1 / 4;
+
+        System.out.println(media);
+        int resultado = media;
+
+        System.out.println(resultado);
+        System.out.println("Resultado é Igual a 6");
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
